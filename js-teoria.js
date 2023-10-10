b6983f7d06519cc2eaba3c794575dc4259ca6dd0
andrea-1987 <142450665+andrea-1987@users.noreply.github.com>
2023-10-10 19:42:40 +0200

    js-teoria

---
+++ js-teoria.js
@@ -0,0 +1,8 @@
+let saluto = "ciao"
+alert(saluto)
+let nome = prompt ("inserisci il tuo nome")
+let messaggio = "Ciao " + nome
+alert(messaggio + "vuoi saperne di piu su JavaScript?")
+alert("vuoi sapere cosa sono algoritmi e variabili?")
+document.write (messaggio + "! Javascript è un linguaggio nato per funzionare nel browser.Un insieme di algoritmi e variabili.Un algoritmo è un insieme di procedure attue ad arrivare al risultato finale.Le variabili sono risultati parziali della procedura per raggiungere il risultato finale: hanno nomi unici, sono riutilizzabili e hanno 'scope' limitato;ovvero quando vengono dichiarate servono al completamento relativo al codice inserito.Dichiarare una variabile dunque signifa assegnargli un valore.Fino a quando non assegnamo un valore alla variabile sarà 'undefined' e si differenzia da 'null' che è invece il valore impostato alla variabile.")
+console.log
